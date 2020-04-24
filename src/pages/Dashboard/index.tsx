79884588b5e2a9b204950b0e8d660ac3e8745595
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-04-24 08:02:23 +0100

    unique commit

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,67 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@githubExplorer:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@githubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    e: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    e.preventDefault();
+    if (!newRepo) {
+      setInputError('Insert author/name of repository');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Repository not found');
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="logo" />
       <Title>Explore repositories in GitHub</Title>
-      <Form>
-        <input placeholder="Write the repository name" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Insert the repository name"
+        />
         <button type="submit">Search</button>
       </Form>
+      {inputError && <Error>{inputError}</Error>}
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43707021?s=460&u=2091ef07e596a03e33f05bb5088614672bb29985&v=4"
-            alt="Thiago Tavares"
-          />
-          <div>
-            <strong>tltavares/rocket</strong>
-            <p>Easy peasy high scalable an available</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43707021?s=460&u=2091ef07e596a03e33f05bb5088614672bb29985&v=4"
-            alt="Thiago Tavares"
-          />
-          <div>
-            <strong>tltavares/rocket</strong>
-            <p>Easy peasy high scalable an available</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43707021?s=460&u=2091ef07e596a03e33f05bb5088614672bb29985&v=4"
-            alt="Thiago Tavares"
-          />
-          <div>
-            <strong>tltavares/rocket</strong>
-            <p>Easy peasy high scalable an available</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43707021?s=460&u=2091ef07e596a03e33f05bb5088614672bb29985&v=4"
-            alt="Thiago Tavares"
-          />
-          <div>
-            <strong>tltavares/rocket</strong>
-            <p>Easy peasy high scalable an available</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -10,7 +14,7 @@ export const Title = styled.h1`
   margin-top: 10px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -22,6 +26,19 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
   }
 
   button {
@@ -71,7 +88,8 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 16px;
+      margin: 0 16px;
+      flex: 1;
 
       strong {
         font-size: 20px;
@@ -90,4 +108,10 @@ export const Repositories = styled.div`
       color: #cbcbcb;
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  margin-top: 8px;
+  color: #c53020;
 `;
